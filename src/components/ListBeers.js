0e3460b5318fce9c6869117e565b740013d09992
Silvia Ferrai <silviafer@hotmail.it>
2022-07-21 17:32:17 +0200

    Added search bar

--- src/components/ListBeers.js
+++ src/components/ListBeers.js
@@ -4,6 +4,7 @@ import { getAllBeers } from '../services/api';
 
 export function ListBeers() {
     const [beers, setBeers] = useState([]);
+    const [keyword, setKeyword] = useState('');
 
     useEffect(() => {
         getAllBeers().then((resp) => {
@@ -11,25 +12,57 @@ export function ListBeers() {
         });
     }, []);
 
+    const handleChange = (e) => {
+        setKeyword(e.target.value);
+    };
+
     return (
-        <div className="grid-container">
-            {beers.map((beer) => {
-                return (
-                    <div key={beer.id} className="beer-container">
-                        <img
-                            className="beer-img"
-                            src={beer.image_url}
-                            alt={beer.name}
-                        />
-                        <div className="beer-info">
-                            <h3 className="beer__title">{beer.name}</h3>
-                            <h4 className="beer__title--sub">{beer.tagline}</h4>
-                            <span>{beer.abv}%</span>
-                            <p>First brewed: {beer.first_brewed}</p>
-                        </div>
-                    </div>
-                );
-            })}
-        </div>
+        <>
+            <form
+                onSubmit={(e) => {
+                    e.preventDefault();
+                }}
+            >
+                <input
+                    type="text"
+                    placeholder="Search"
+                    value={keyword}
+                    onChange={handleChange}
+                />
+            </form>
+            <div className="grid-container">
+                {beers
+                    .filter((beer) => {
+                        if (keyword === '') {
+                            return beer;
+                        } else if (
+                            beer.name
+                                .toLowerCase()
+                                .includes(keyword.toLowerCase())
+                        ) {
+                            return beer;
+                        }
+                    })
+                    .map((beer) => {
+                        return (
+                            <div key={beer.id} className="beer-container">
+                                <img
+                                    className="beer-img"
+                                    src={beer.image_url}
+                                    alt={beer.name}
+                                />
+                                <div className="beer-info">
+                                    <h3 className="beer__title">{beer.name}</h3>
+                                    <h4 className="beer__title--sub">
+                                        {beer.tagline}
+                                    </h4>
+                                    <span>{beer.abv}%</span>
+                                    <p>First brewed: {beer.first_brewed}</p>
+                                </div>
+                            </div>
+                        );
+                    })}
+            </div>
+        </>
     );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,6 +4,7 @@ import { ListBeers } from '../components/ListBeers';
 export function Home() {
     return (
         <div>
+            <h1>Punk API</h1>
             <ListBeers />
         </div>
     );
