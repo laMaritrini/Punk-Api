24f66e0fae6a2975039a74fdf69f1d6b8a3b160a
Silvia Ferrai <silviafer@hotmail.it>
2022-07-22 21:00:28 +0200

    Added css in Home component

--- src/components/ListBeers.js
+++ src/components/ListBeers.js
@@ -13,44 +13,38 @@ export function ListBeers({ keyword }) {
     }, []);
 
     return (
-        <>
-            <div className="grid-container">
-                {beers
-                    .filter((beer) => {
-                        if (keyword === '') {
-                            return beer;
-                        } else if (
-                            beer.name
-                                .toLowerCase()
-                                .includes(keyword.toLowerCase())
-                        ) {
-                            return beer;
-                        }
-                    })
-                    .map((beer) => {
-                        return (
-                            <div key={beer.id} className="beer-container">
-                                <Link to={`/details/${beer.id}`}>
-                                    <img
-                                        className="beer-img"
-                                        src={beer.image_url}
-                                        alt={beer.name}
-                                    />
-                                    <div className="beer-info">
-                                        <h3 className="beer__title">
-                                            {beer.name}
-                                        </h3>
-                                        <h4 className="beer__title--sub">
-                                            {beer.tagline}
-                                        </h4>
-                                        <span>{beer.abv}%</span>
-                                        <p>First brewed: {beer.first_brewed}</p>
-                                    </div>
-                                </Link>
-                            </div>
-                        );
-                    })}
-            </div>
-        </>
+        <div className="grid-container">
+            {beers
+                .filter((beer) => {
+                    if (keyword === '') {
+                        return beer;
+                    } else if (
+                        beer.name.toLowerCase().includes(keyword.toLowerCase())
+                    ) {
+                        return beer;
+                    }
+                })
+                .map((beer) => {
+                    return (
+                        <div key={beer.id} className="beer-container">
+                            <Link to={`/details/${beer.id}`}>
+                                <img
+                                    className="beer-img"
+                                    src={beer.image_url}
+                                    alt={beer.name}
+                                />
+                                <div className="beer-info">
+                                    <h3 className="beer__title">{beer.name}</h3>
+                                    <h4 className="beer__title--sub">
+                                        {beer.tagline}
+                                    </h4>
+                                    <span>{beer.abv}%</span>
+                                    <p>First brewed: {beer.first_brewed}</p>
+                                </div>
+                            </Link>
+                        </div>
+                    );
+                })}
+        </div>
     );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { ListBeers } from '../components/ListBeers';
+import './Home.css';
 
 export function Home() {
     const [keyword, setKeyword] = useState('');
@@ -7,21 +8,37 @@ export function Home() {
         setKeyword(e.target.value);
     };
     return (
-        <div>
-            <h1>Punk API</h1>
-            <form
-                onSubmit={(e) => {
-                    e.preventDefault();
-                }}
-            >
-                <input
-                    type="text"
-                    placeholder="Search"
-                    value={keyword}
-                    onChange={handleChange}
-                />
-            </form>
+        <>
+            <div className="home">
+                <h1 className="title-app">Punk API</h1>
+                <form
+                    className="input-container"
+                    onSubmit={(e) => {
+                        e.preventDefault();
+                    }}
+                >
+                    <input
+                        className="input-search"
+                        type="text"
+                        placeholder="Search"
+                        value={keyword}
+                        onChange={handleChange}
+                    />
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="input-icon"
+                        viewBox="0 0 20 20"
+                        fillRule="currentColor"
+                    >
+                        <path
+                            fillRule="evenodd"
+                            d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                            clipRule="evenodd"
+                        />
+                    </svg>
+                </form>
+            </div>
             <ListBeers keyword={keyword} />
-        </div>
+        </>
     );
 }
