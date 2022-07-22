550d6f5d8e00b90cdb8ff983b58a984fe8d1206e
Silvia Ferrai <silviafer@hotmail.it>
2022-07-22 21:19:11 +0200

    Added css in Details component

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useParams } from 'react-router-dom';
 import { getBeer } from '../services/api';
+import './Details.css';
 
 export function Details() {
     const [beerDetail, setBeerDetail] = useState('');
